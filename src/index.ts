2d9f28cd5b4d35d4b9933fb7af5b1694c2941246
KIMINJOONG <ijboym216@gmail.com>
2019-12-13 09:16:45 +0900

    1.10 Configuring TypeORM

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Options } from "graphql-yoga";
 import app from "./app";
+import { createConnection } from "typeorm";
+import connectionOptions from "./ormConfig";
 
 const PORT: number | string = process.env.PORT || 4000;
 const PLAYGROUND: string = "/playground";
@@ -14,4 +16,7 @@ const appOptions: Options = {
 const handleAppStart = () => {
   console.log(`listening port ${PORT}`);
 };
-app.start(appOptions, handleAppStart);
+
+createConnection(connectionOptions).then(() => {
+  app.start(appOptions, handleAppStart);
+});
