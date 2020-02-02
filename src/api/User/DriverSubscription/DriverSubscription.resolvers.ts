b50184e95f43427900b30f5bb0710cf5b8c4b82d
KIMINJOONG <ijboym216@gmail.com>
2020-02-03 08:26:54 +0900

    1.70 Filtering Subscription Messages

--- src/api/User/DriverSubscription/DriverSubscription.resolvers.ts
+++ src/api/User/DriverSubscription/DriverSubscription.resolvers.ts
@@ -1,9 +1,28 @@
+import { withFilter } from "graphql-yoga";
+import User from "src/entities/User";
+
 const resolvers = {
   Subscription: {
     DriverSubscription: {
-      subscribe: (_, __, { pubSub }) => {
-        return pubSub.asyncIterator("driverUpdate");
-      }
+      subscribe: withFilter(
+        (_, __, { pubSub }) => pubSub.asyncIterator("driverUpdate"),
+        (payload, _, { context }) => {
+          const user: User = context.currentUser;
+          const {
+            DriverSubscription: {
+              lastLat: driverLastLat,
+              lastLng: driverLastLng
+            }
+          } = payload;
+          const { lastLat: userLastLat, lastLat: userLastLng } = user;
+          return (
+            driverLastLat >= userLastLat - 0.05 &&
+            driverLastLat <= userLastLat + 0.05 &&
+            driverLastLng >= userLastLng - 0.05 &&
+            driverLastLng <= userLastLng + 0.05
+          );
+        }
+      )
     }
   }
 };
